bc3839493903a918e29a39d870df3014981af1eb
Nariman Khaleghi <54586611+NarimanKhaleghi@users.noreply.github.com>
2026-02-27 22:42:34 +0330

    Add service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'etebar-man-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.0.0/Vazirmatn-font-face.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Install Event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+            // Offline fallback strategy can be implemented here
+        });
+      }
+    )
+  );
+});
+
+// Activate Event (Clear old caches)
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
